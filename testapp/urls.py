3f45e7f21c099c30b4fd7931ccf4b938cc619456
yoonje-c <yoonje.c@worksmobile.com>
2020-10-11 01:13:56 +0900

    remove unusable files

--- testapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, re_path
-from . import views
-
-urlpatterns = [
-    path('', views.show_home_page, name='show_home_page'),
-]
--- testapp/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def show_home_page(request):
-    return render(request, 'viewer.html', {})
